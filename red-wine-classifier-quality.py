6ead0d71553e9479cfbbe9a2a6cff8666915d8c3
Gabriel Eugenio <gabriell.eugenio@gmail.com>
2020-11-29 22:12:20 -0300

    Update red-wine-classifier-quality.py

--- red-wine-classifier-quality.py
+++ red-wine-classifier-quality.py
@@ -64,8 +64,44 @@ dfm8 = resample(df_q8 ,
 # creating dataset balaced
 df = pd.concat([df_maj , dfm3 , dfm4 , dfm7 , dfm8])
 
-# 
+# separate features and labels
 features = df.drop('quality' , axis = 1)
 label = df['quality']
 
+# normalizing dataset features
 scaler_atr = StandardScaler()
+
+atb = scaler_atr.fit_transform(features)
+
+X = np.matrix(atb)
+S = np.cov(np.transpose(X)) 
+
+# generate PCA components and adding in features 
+pca = PCA(n_components=8)
+
+pca.fit(X)
+
+components = np.round(pca.explained_variance_ratio_ , 2)
+
+pca_1 = pca.transform(X)[:,0]
+pca_2 = pca.transform(X)[:,1]
+pca_3 = pca.transform(X)[:,2]
+pca_4 = pca.transform(X)[:,3]
+pca_5 = pca.transform(X)[:,4]
+pca_6 = pca.transform(X)[:,5]
+pca_7 = pca.transform(X)[:,6]
+pca_8 = pca.transform(X)[:,7]
+
+df['PCA1'] = pca_1
+df['PCA2'] = pca_2
+df['PCA3'] = pca_3
+df['PCA4'] = pca_4
+df['PCA5'] = pca_5
+df['PCA6'] = pca_6
+df['PCA7'] = pca_7
+df['PCA8'] = pca_8
+
+# plotting new correlation
+matrix_corr = df.corr()
+matrix_corr = matrix_corr['quality'].sort_values(ascending=False)
+
