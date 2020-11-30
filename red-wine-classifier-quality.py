64e39ed1d2450da5c028f56bf06371e8a6f06061
Gabriel Eugenio <gabriell.eugenio@gmail.com>
2020-11-29 22:08:03 -0300

    Update red-wine-classifier-quality.py

--- red-wine-classifier-quality.py
+++ red-wine-classifier-quality.py
@@ -28,5 +28,44 @@ sns.heatmap(matrix_corr,
 # creating new column
 df['total acidity'] = df['fixed acidity'] + df['volatile acidity']
 
+# plotting count values of quality labels
+sns.countplot(x='quality', data=df)
 
+# creating dataframe with bigest count count values 
+max_samples = len(df[df['quality']== 5])
+df_maj = df[(df['quality'] == 5) | (df['quality'] == 6)]
 
+# resampling 3,4,7 and 8 values of quality, to balacing dataset 
+df_q3 = df[df['quality'] == 3]
+df_q4 = df[df['quality'] == 4]
+df_q7 = df[df['quality'] == 7]
+df_q8 = df[df['quality'] == 8]
+
+dfm3 = resample(df_q3 ,
+                replace = True,
+                n_samples= max_samples,
+                random_state=0)
+
+dfm4 = resample(df_q4 ,
+                replace = True,
+                n_samples= max_samples,
+                random_state=0)
+
+dfm7 = resample(df_q7 ,
+                replace = True,
+                n_samples= max_samples,
+                random_state=0)
+
+dfm8 = resample(df_q8 ,
+                replace = True,
+                n_samples= max_samples,
+                random_state=0)
+
+# creating dataset balaced
+df = pd.concat([df_maj , dfm3 , dfm4 , dfm7 , dfm8])
+
+# 
+features = df.drop('quality' , axis = 1)
+label = df['quality']
+
+scaler_atr = StandardScaler()
